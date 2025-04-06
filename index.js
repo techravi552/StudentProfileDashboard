4e0dd83fd2e44ba29f132b4e3a4335c16e4c01e0
Ravi Kalmodiya <ravikalmodiya552gamil.com>
2025-04-06 12:44:51 +0530

    student dasbord  firebase

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+fetch("https://studentprofile-2e442-default-rtdb.asia-southeast1.firebasedatabase.app/Students.json")
+.then((res)=> res.json()
+    
+).then((product) =>{
+    console.log(product);
+    
+})
