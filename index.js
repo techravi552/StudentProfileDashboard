88984eb9a67d7876fc441500a193b0e25193d9b6
Ravi Kalmodiya <ravikalmodiya552gamil.com>
2025-04-06 13:35:20 +0530

    student dasbord  firebase

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ fetch("https://studentprofile-2e442-default-rtdb.asia-southeast1.firebasedatabas
     console.log(product);
     
 })
+
+async function fetchData() {
+    res =  await fetch("https://studentprofile-2e442-default-rtdb.asia-southeast1.firebasedatabase.app/Students.json")
+    let Students = await res.json()
+    console.log(Students)
+
+    let studentArray = Object.entries(Students).map(([id , Students])=>({id , ...Students}))
+    // console.log("studentArray");
+    
+
+    studentArray.map((e)=>{
+       console.log("ravi" , e.name)
+       tabledata = document.getElementById("tabledata")
+    
+      tr=document.createElement("tr")
+      tr.innerHTML=`<td>${e.name}</td><td>${e.Email}</td> <td>${e.age}</td> <td>${e.Course}</td> <td>${e.Skills}</td></td> <td>${e.Gender}</td>`
+      tabledata.append(tr)
+
+    })
+}
+fetchData()
